9558afeb042f8fe5f737e68b4c0d3af044c8c540
Kirill Rabtsevich <kirill500123@gmail.com>
2023-09-24 17:23:24 +0300

    finally fixed

--- homework-3/task-3.py
+++ homework-3/task-3.py
@@ -9,22 +9,24 @@ def find_min(arr):
     left = 0
     right = len(arr) - 1
 
-    mid = (left + right) >> 1
-    if len(arr) > 2 and arr[left] == arr[right] == arr[mid]:
-        minimal = arr[0]
-        for i in range(1, len(arr)):
-            if arr[i] < minimal:
-                minimal = arr[i]
-        return minimal
-
     while left < right:
         mid = (left + right) >> 1
-        if arr[left] > arr[mid]:
-            right = mid
-        elif arr[right] < arr[mid]:
-            left = mid + 1
+        if right - left > 2 and arr[left] == arr[right] == arr[mid]:
+            minimal = arr[0]
+            for i in range(left, mid):
+                if arr[i] < minimal:
+                    minimal = arr[i]
+            if minimal == arr[0]:
+                left = mid
+                continue
+            return minimal
         else:
-            return arr[left]
+            if arr[left] > arr[mid]:
+                right = mid
+            elif arr[right] < arr[mid]:
+                left = mid + 1
+            else:
+                return arr[left]
     return arr[left]
 
 
@@ -36,3 +38,5 @@ print(find_min([4, 5, 6, 7, 8, 100, -1, 0]))
 print(find_min([4, 5, 6, 7, 8, 100, 109, -1]))
 print(find_min([1, 3, 4, 5, 6, 7, 8, 9]))
 print(find_min([7, 10, 2, 4, 5, 6]))
+print(find_min([0, 5, 5, 5, 5, 5]))
+print(find_min([5, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5]))
